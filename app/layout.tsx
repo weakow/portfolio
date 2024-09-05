21b6271c8b6a19086d8d51b44b993fd8c49600e2
weakow <w@weakow.com>
2024-09-05 12:36:48 +0800

    Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,15 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          type="image/png"
+        />
+      </head>
       <body className="antialiased">
         <main className="flex-auto">
           {children}
